4355c312fde41f15c27228450ac0b566e436c089
DasMonopol <unimail.catoka@gmail.com>
2019-04-27 16:30:00 +0200

    Changed Naming of Invoice and added Reasons

--- Assets/Scripts/Invoice/InvoiceData.cs
+++ Assets/Scripts/Invoice/InvoiceData.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Invoice", menuName = "Invoice/New Invoice")]
-public class InvoiceScriptable : ScriptableObject
+[System.Serializable]
+public class InvoiceData
 {
-    [SerializeField] private string m_Reason;
+    [SerializeField] private InvoiceReasons m_Reason;
     [SerializeField] private float m_Price;
     [SerializeField] private int m_Duration;
     [SerializeField] private bool m_IsSigned;
 
-    public string Reason { get => m_Reason; set => m_Reason = value; }
+    public InvoiceReasons Reason { get => m_Reason; set => m_Reason = value; }
     public float Price { get => m_Price; set => m_Price = value; }
     public int Duration { get => m_Duration; set => m_Duration = value; }
     public bool IsSigned { get => m_IsSigned; set => m_IsSigned = value; }
+
+    public InvoiceData(InvoiceReasons reason, float price, int duration)
+    {
+        m_Reason = reason;
+        m_Price = price;
+        m_Duration = duration;
+    }
 }
---
+++ Assets/Scripts/Invoice/InvoiceReasons.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Reason", menuName = "Invoice/New Reason")]
+public class InvoiceReasons : ScriptableObject
+{
+    [TextArea(0, 50)]
+    [SerializeField] private string m_Text = null;
+
+    public string Text { get => m_Text; private set => m_Text = value; }
+}
