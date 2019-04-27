b9e6589c634125a9f3c2ca0600dc38e2eb202a5a
DasMonopol <unimail.catoka@gmail.com>
2019-04-27 14:27:34 +0200

    Added TextMeshPro and Invoice Prototype

---
+++ Assets/Scripts/Rechnung/InvoiceScriptable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Invoice", menuName = "Invoice/New Invoice")]
+public class InvoiceScriptable : ScriptableObject
+{
+    [SerializeField] private string m_Reason;
+    [SerializeField] private float m_Price;
+    [SerializeField] private int m_Duration;
+
+    public string Reason { get => m_Reason; set => m_Reason = value; }
+    public float Price { get => m_Price; set => m_Price = value; }
+    public int Duration { get => m_Duration; set => m_Duration = value; }
+}
---
+++ Assets/Scripts/Rechnung/InvoiceUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InvoiceUI : MonoBehaviour
+{
+    [SerializeField] private InvoiceScriptable m_InvoiceScriptable;
+    [Space]
+    [SerializeField] private TextMeshProUGUI m_Reason;
+    [SerializeField] private TextMeshProUGUI m_Cost;
+    [SerializeField] private TextMeshProUGUI m_Duration;
+    [SerializeField] private Button m_SignatureButton;
+    [SerializeField] private GameObject m_Signature;
+    [Space]
+    [SerializeField] private bool m_IsSigned;
+
+    public InvoiceScriptable InvoiceScriptable { get => m_InvoiceScriptable; set => m_InvoiceScriptable = value; }
+
+    private void Start()
+    {
+        if (InvoiceScriptable == null)
+        {
+            return;
+        }
+
+        m_Reason.text = m_InvoiceScriptable.Reason;
+        m_Cost.text = m_InvoiceScriptable.Price.ToString();
+        m_Duration.text = m_InvoiceScriptable.Duration.ToString();
+    }
+
+    public void SignInvoice()
+    {
+        m_IsSigned = true;
+
+        m_Signature.SetActive(true);
+
+        m_SignatureButton.enabled = false;
+    }
+}
