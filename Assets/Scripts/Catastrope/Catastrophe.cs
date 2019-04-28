ea1ed98949566af79dd51d721d2ef2616bada604
DasMonopol <unimail.catoka@gmail.com>
2019-04-28 13:22:57 +0200

    Added more logic to the Catastrophe scripts and created the UI display logic for Catastrophes

--- Assets/Scripts/Catastrope/Catastrophe.cs
+++ Assets/Scripts/Catastrope/Catastrophe.cs
@@ -1,13 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Catastrophe : MonoBehaviour, ICatastrophe
 {
     [SerializeField] private CatastropheData m_CatastropheData = null;
+    [Space]
+    [SerializeField] private TextMeshProUGUI m_AppName = null;
+    [SerializeField] private TextMeshProUGUI m_Kills = null;
+    [SerializeField] private TextMeshProUGUI m_Cooldown = null;
+    [SerializeField] private Image m_Icon = null;
 
     public CatastropheData CatastropheData { get => m_CatastropheData; set => m_CatastropheData = value; }
 
+    [ContextMenu("Initialize")]
+    public void Initialize()
+    {
+        if (m_CatastropheData == null)
+        {
+            return;
+        }
+
+        m_AppName.text = m_CatastropheData.CatastropheName;
+        m_Kills.text = $"Kills: {m_CatastropheData.MinimumKills} - {m_CatastropheData.MaximumKills}";
+        m_Cooldown.text = m_CatastropheData.Cooldown.ToString();
+    }
+
     public void StartCatastrophe()
     {
-        Debug.LogWarning("Scoopidi Whoop. Whoopidi Scoop. Scipoop poopidipoop.");
+        Debug.Log("SKIGI");
+        Invoke("Yeet", 4f);
+    }
+
+    private void Yeet()
+    {
+        Debug.Log("YOOOT");
     }
 }
--- Assets/Scripts/Catastrope/CatastropheData.cs
+++ Assets/Scripts/Catastrope/CatastropheData.cs
@@ -3,7 +3,7 @@
 [CreateAssetMenu(fileName = "New Catastrophe", menuName = "Catastrophe/New Catastrophe")]
 public class CatastropheData : ScriptableObject
 {
-    [SerializeField] private string m_Name = null;
+    [SerializeField] private string m_CatastropheName = null;
     [SerializeField] private bool m_IsUnlocked = false;
     [SerializeField] private int m_Price = 0;
     [SerializeField] private float m_Duration = 0f;
@@ -13,7 +13,7 @@ public class CatastropheData : ScriptableObject
     [Range(0, 1)]
     [SerializeField] private float m_CritChance = 0f;
 
-    public string Name { get => m_Name; set => m_Name = value; }
+    public string CatastropheName { get => m_CatastropheName; set => m_CatastropheName = value; }
     public bool IsUnlocked { get => m_IsUnlocked; set => m_IsUnlocked = value; }
     public int Price { get => m_Price; set => m_Price = value; }
     public float Duration { get => m_Duration; set => m_Duration = value; }
