610914f0d5a395ffca1aec94eb6424de009eef8f
DasMonopol <unimail.catoka@gmail.com>
2019-04-30 00:09:58 +0200

    Implemented the opening and closing of the smartphone

---
+++ Assets/Scripts/Catastrope/CatastrophePhone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatastrophePhone : MonoBehaviour
+{
+    public void OpenPhone()
+    {
+        this.transform.GetChild(0).transform.gameObject.SetActive(true);
+    }
+
+    public void ClosePhone()
+    {
+        this.transform.GetChild(0).transform.gameObject.SetActive(false);
+    }
+}
